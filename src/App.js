4629d3a8d7798615571fcebace3a724247c43e24
Adam-Young <awy6448@gmail.com>
2020-06-19 14:04:36 -0400

    stoped using state managment, added a button to grab a new quote.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios"
 
 function App() {
+const [quote, setQuote] = useState({})
+const getQuote = () => {
+  axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+      .then(res => {
+        setQuote(res.data)
+      })
+}
+  useEffect(() => {
+    getQuote()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ron Quotes to live by</h1>
+        <span>{quote[0]}</span>
+        <button onClick={() => {getQuote()}} >New Quote</button>
       </header>
     </div>
   );
